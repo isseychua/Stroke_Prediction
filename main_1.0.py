7f987e617b751538fba5ebf62d170899ac9c572f
IsseyChua <isseychua@gmail.com>
2022-01-09 16:47:22 +0800

    Keep age,avg_glucose_level,stroke features only

--- main_1.0.py
+++ main_1.0.py
@@ -32,7 +32,6 @@ Main.drop_outliers()
 print(Main.df.shape)
 
 #EDA
-#
 #Create dataframe for positive stroke
 Main.Create_positive_stroke_df("stroke")
 #Uni Variate analysis
@@ -43,11 +42,18 @@ Main.bivariate_num(2,4)
 Main.bivariate_cat(2,8)
 
 #Feature Engineering
-#Filter age>36
-Main.Filtered_df(60,90)
+'''
+#Calculate the mean and std of age and avg_glucose_level
+Main.Mean_Std_Calculation()
+#Filter age>36, avg_glucose_level<90
+Main.Filtered_df(Main.Age_Mean-Main.Age_Std*0.5,Main.Glucose_Mean+Main.Glucose_Std*0.5)
+'''
+#Keep Age and BMI feature
+Main.Filter_columns()
+'''
 #Label Encoder
 Main.Label_Encoder(Main.Type_Obj)
-
+'''
 #Modelling
 Main.Initiate_ML_Algo()
 Main.Create_Result_Table(Main.Result_Table_Column)
@@ -61,5 +67,5 @@ Main.Ave_Score(Main.Result_Table_Column)
 print(Main.Result_Table)
 
 #Export table of results in csv
-Main.Export_Results_Csv("results_filtered_age_above_60_gluclose_below_90_ros.txt",False)
+Main.Export_Results_Csv("results_filtered_age_glucose_ros.txt",False)
 
--- packages/feature_engineering_mod.py
+++ packages/feature_engineering_mod.py
@@ -11,7 +11,21 @@ class featureengineering_class():
 
     def Create_positive_stroke_df(self,Target):
         self.stroke_df=self.df[self.df[Target]==1]
-
+    
+    def Filter_columns(self):
+        self.df=self.df[["age","avg_glucose_level","stroke"]]
+    '''
     def Filtered_df(self,age_limit,glucose_limit):
         self.df=self.df[(self.df["age"]>age_limit) & (self.df["avg_glucose_level"]<glucose_limit)]
         print(self.df.shape)
+    
+    def Mean_Std_Calculation(self):
+        self.Age_Mean=self.stroke_df["age"].mean()
+        self.Age_Std=self.stroke_df["age"].std()
+        self.Glucose_Mean=self.stroke_df["avg_glucose_level"].mean()
+        self.Glucose_Std=self.stroke_df["avg_glucose_level"].std()
+        print(self.Age_Mean)
+        print(self.Age_Std)
+        print(self.Glucose_Mean)
+        print(self.Glucose_Std)
+    '''
